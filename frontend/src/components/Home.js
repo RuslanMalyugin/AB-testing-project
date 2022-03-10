7cbc148da69070ef96291dadbdbdbb07af927695
Елизавета Неледова <neledova.ev@phystech.edu>
2022-03-10 23:53:30 +0300

    Make top and begin bottom

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,34 +1,107 @@
 import React from 'react';
 import Header from './Header';
 import './Home.css';
-import family from './imgs/FreeMan.png'
-import Footer from "./Footer";
+import plotleft from "./imgs/plotleft.png";
+import plotright from "./imgs/plotright.png";
 
+
+function handleSubmit() {
+    alert('Что-то');
+}
 
 function Home() {
     return (
         <body>
         <Header/>
         <main className='Main'>
-            <div className='Banner'>
+            <div className='BannerLeft'>
                 <div className='BannerTop'>
                     СЛОГАН <br/>
                     СЛОГАН <br/>
                     СЛОГАН <br/>
                     СЛОГАН <br/>
                 </div>
-                <div className='BannerDown'>
-                    Наш сервис - для тебя! <br/>
-                    Проходи простую регистрацию и оставляй свой заказ.
-                </div>
                 {/*<a className='Reg' href='/login'>*/}
                 {/*    Зарегистрироваться*/}
                 {/*</a>*/}
             </div>
-            <div>
+            <div className='BannerRight'>
+                <div className='Text'>
+                    A/B-testing platform <br/>
+                </div>
+                <div className='Button'>
+                    <div className='SmallText'>
+                        Приступить к работе <br/>
+                    </div>
+                </div>
+                    <div className='ButtonMore'>
+                        <div className='SmallText'>
+                            <a href="#about">
+                                Узнать больше <br/>
+                            </a>
+                        </div>
+                    </div>
             </div>
         </main>
-        <Footer/>
+
+
+        <a name="about"></a>
+        <div className='BannerDown'>
+            <div className='SmallText'>
+                текст с описанием текст с описанием текст с описанием текст с описанием текст с описанием <br/>
+                текст с описанием текст с описанием текст с описанием <br/>
+            </div>
+        </div>
+
+        <main className='Main'>
+            <div className='BannerRight'>
+                <div className='MiddleText'>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                    А/В-тестирование круто и очень полезно <br/>
+                </div>
+                <div className='Picture'>
+                    <img src={plotright} width={250}/>
+                </div>
+            </div>
+            <div className='BannerRightDown'>
+                <div className='Picture'>
+                    <img src={plotleft} width={250}/>
+                </div>
+                <div className='BannerSystemInfo'>
+                    <ul className="Points" type="circle">
+                        <li className="Point">
+                            Мы помогаем студентам МФТИ справиться с проблемами с успеваемостью по физ-культуре.
+                        </li>
+                        <li className="Point">
+                            На сайте вы можете оставить свою заявку о нужной дате посещения, и другие студенты могут взять ваш заказ.
+                        </li>
+
+                        <li className="Point">
+                            Гарантируем анонимность до момента подтверждения заказа.
+                        </li>
+
+                        <li className="Point">
+                            Идея по созданию данного сервиса пришла во время очередной зачетной недели помимо учебных предметов приходилось закрывать в спешке физ-культуру, а так же историями о потерянной стипендии из-за отсутствия нескольких посещений.
+                        </li>
+
+                        <li className="Point">
+                            Если у вас остались вопросы вы можете связаться с нами, все контакты находятся внизу
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+
+        </main>
+
         </body>
     );
 }
