1ee5f197ff53b944791eb0990f8c6e3ce30e45a4
Naimdzhon Khondzhonov <khondzhonov.nd@phystech.edu>
2022-03-21 21:51:21 +0000

    [ABT-6] module splitter edited

--- splitter.py
+++ splitter.py
@@ -1,23 +1,50 @@
-import random
-import string
+import sys
 import pandas as pd
+import os
+from base64 import b64encode
+import hashlib
 
 
-def split(users_id, w_1=0.5):
-    df = pd.DataFrame({'users_id': users_id})
+def encode_id(x: str, salt: str) -> int:
+    """
+    Generate hash ids using prefered salt value and sha256 algorithm.
+    Arguments
+    ---------
+    :x: str
+        Encoding string
+    :salt: str
+        Salt for endoing (x + salt)
+
+    Returns
+    -------
+    :x_hashed: int - hash of x
+    """
+    x = str(x)
+    x += salt
+    x = x.encode()
+    x_hashed = hashlib.sha256(x)
+    x_hashed = x_hashed.hexdigest()
+    x_hashed = x_hashed[:10]
+    x_hashed = int(x_hashed, 16)
+    return x_hashed
+
+
+def split(id_column, salt=None, w_1=0.5):
+    df = pd.DataFrame({'id_column': id_column})
 
     # генерируем соль
-    salt = ''.join([random.choice(string.ascii_letters) for i in range(10)])
+    if not salt:
+        salt = b64encode(os.urandom(8)).decode('ascii')
 
     # вычислим хеш-функцию для каждого id + salt
-    df['users_hash'] = [hash(str(id) + salt) for id in users_id]
+    df['hashed_id'] = [encode_id(x, salt) % sys.maxsize for x in df['id_column']]
 
     # сортируем пользователей по хешу
-    df.sort_values(by='users_hash', inplace=True)
+    df.sort_values(by='hashed_id', inplace=True)
 
     # разбиваем пользователей на группы A и B с весами w_1 и w_2 (где: w_1 + w_2 = 1)
-    num_a = int(w_1 * users_id.size)
-    test_group = df['users_id'][:num_a].values
-    control_group = df['users_id'][num_a:].values
+    num_a = int(w_1 * id_column.size)
+    test_group = df['id_column'][:num_a].values
+    control_group = df['id_column'][num_a:].values
 
     return test_group, control_group
