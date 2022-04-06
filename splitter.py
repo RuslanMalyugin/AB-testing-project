4632b33bc41e3e50380c2ef25ec14922c6026dc1
Naimdzhon Khondzhonov <khondzhonov.nd@phystech.edu>
2022-04-06 16:40:04 +0000

    [ABT-22] stratified splitting

--- splitter.py
+++ splitter.py
@@ -4,7 +4,7 @@ import os
 from base64 import b64encode
 import hashlib
 import numpy as np
-
+import math
 
 def encode_id(x: str, salt: str) -> int:
     """
@@ -43,15 +43,20 @@ class Splitter(object):
     Methods:
     ---------
     split()
-        делит пользователей группы test и control,
+        делит пользователей на группы test и control,
         возращает id-шники каждой из групп
-        
+    stratifed_split()
+        делит пользователей на группы test и control,
+        методом стратификации и
+        возращает id-шники каждой из групп
+        P.S.: текущая версия метода работает только для 
+              случая w_1=0.5
     """
     
     def __init__(self, data, test_group_size, w_1=0.5):
         
         self.data = data
-        self.sample_size = int(float(test_group_size) / w_1)
+        self.sample_size = math.ceil(float(test_group_size) / w_1)
         self.w_1 = w_1
     
     def split(self, salt=None):
@@ -75,4 +80,30 @@ class Splitter(object):
         control_group = df['id_column'][num_a:].values
 
         return test_group, control_group
+    
+    def stratifed_split(self, covariate):
+        df = self.data[['id_column', covariate]]
+        
+        #мно-во знаечений ковариаты
+        cnt = set(df[covariate])
+        
+        #массив id-шников тестовой и контрольной группы
+        test_group = np.array([])
+        control_group = np.array([])
+        
+        #пройдёмся по каждой группе, который 
+        #составленны из равных значений ковариаты
+        for item in cnt:
+            new_df = df[df[covariate] == item]
+            
+            #кол-во польз-ей с ковариатой item в каждой из групп
+            subgroup_size  = int((self.sample_size * new_df.shape[0] / df.shape[0] + 1) / 2)
+            
+            new_id_list = np.random.choice(new_df['id_column'], (2, subgroup_size), replace = False)
+            test_group = np.concatenate((test_group, new_id_list[0]))
+            control_group = np.concatenate((control_group, new_id_list[1]))
+            
+        return test_group, control_group
+        
 
+     
